5a5315b756d600a19f2e11ac239c8c3f5803598b
skmuduli92 <smuduli@iitk.ac.in>
2021-03-10 21:38:45 +0530

    adding missing ecc test

---
+++ fw/aes_test_ecc_unsafe.c
@@ -0,0 +1,117 @@
+#include <reg51.h>
+
+/*
+ * Copyright (c) 1999-2001 Tony Givargis.  Permission to copy is granted
+ * provided that this header remains intact.  This software is provided
+ * with no warranties.
+ *
+ * Version : 2.9
+ */
+
+/*---------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*/
+
+#define GOOD_ID 0x12
+
+void quit() {
+    P0 = P1 = P2 = P3 = 0xDE;
+    P0 = P1 = P2 = P3 = 0xAD;
+    P0 = P1 = P2 = P3 = 0x00;
+    while(1);
+}
+
+__xdata __at(0xFF00) unsigned char aes_reg_start;
+__xdata __at(0xFF01) unsigned char aes_reg_state;
+__xdata __at(0xFF02) unsigned int aes_reg_addr;
+__xdata __at(0xFF04) unsigned int aes_reg_len;
+__xdata __at(0xFF10) unsigned char aes_reg_ctr[16];
+__xdata __at(0xFF20) unsigned char aes_reg_key0[16];
+__xdata __at(0xFF30) unsigned char aes_reg_key1[16];
+__xdata __at(0xE000) unsigned char data[16];
+
+__xdata __at(0xFF80) unsigned char pt_wren[32];
+__xdata __at(0xFFA0) unsigned char pt_rden[32];
+
+__xdata __at(0xEFFC) unsigned int debug_reg_addr;
+__xdata __at(0xEFFE) unsigned int debug_reg_data;
+
+
+/*---------------------------------------------------------------------------*/
+
+void main() {
+
+    int i;
+    int good=1;
+
+    // enable read/write to all locations in the page tables.
+    for(i=0; i < 32; i++) {
+        pt_wren[i] = 0xff;
+        pt_rden[i] = 0xff;
+    }
+    // test writing to XRAM.
+    for(i=0; i < 32; i++) {
+        data[i]=i;
+    }
+
+    // setup address, length, counter and key.
+    aes_reg_addr = 0xE000;
+    aes_reg_len = 32;
+    for(i=0; i < 16; i++) { aes_reg_ctr[i] = i*i*i; }
+    for(i=0; i < 16; i++) { aes_reg_key0[i] = i | (i << 4); }
+
+    // now start encryption.
+    aes_reg_start = 1;
+    // now wait for encryption to complete.
+    while(aes_reg_state != 0);
+
+    // read encrypted data and dump it to P0.
+    for(i=0; i < 32; i++) {
+        P0 = data[i];
+    }
+
+    // let us decrypt now.
+    aes_reg_start = 1;
+    while(aes_reg_state != 0)  {
+        __asm;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+            nop;
+        __endasm;
+    }
+
+    for(i=0; i < 32; i++) {
+        if(data[i] != i) {
+            good = 2;
+            break;
+        }
+    }
+
+    P0 = good;
+    debug_reg_addr = GOOD_ID;
+    debug_reg_data = good;
+    // finish.
+    quit();
+}
--- fw/secureboot.c
+++ fw/secureboot.c
@@ -13,252 +13,247 @@
 
 #include <8051.h>
 
-
-
 XDATA_ARR(0x0001, MAX_PRG_SIZE, unsigned char, program);
 XDATA_ARR(0x3000, MAX_IM_SIZE, unsigned char, boot);
 
-XDATA_ARR(0xC000, MAX_IM_SIZE+0x40, unsigned char, sha_in);
+XDATA_ARR(0xC000, MAX_IM_SIZE + 0x40, unsigned char, sha_in);
 XDATA_ARR(0xE100, H, unsigned char, sha_out);
 XDATA_ARR(0xE200, N, unsigned char, rsa_out);
+
+/*---------------------------------------------------------------------------*/
+/* DEBUGGIN INFO FOR TRACE GENERATION */
+#define VERIF_HEADER_SIGN 0x01
+#define VERIF_BLOCK_HASH 0x02
+
+__xdata __at(0xEFFC) unsigned int debug_reg_addr;
+__xdata __at(0xEFFE) unsigned int debug_reg_data;
+
+void record_dbg(int ID, unsigned data) {
+  debug_reg_addr = ID;
+  debug_reg_data = data;
+}
 
 /*---------------------------------------------------------------------------*/
 
 #define PACK(var, size) unsigned char var[size]
 
-
-struct modules{
-    unsigned int addr;
-    PACK(p0,2);  // add extra bytes to account for different type sizes
-    unsigned int size;
-    PACK(p1,2);
-    unsigned char hash[H];
+struct modules {
+  unsigned int addr;
+  PACK(p0, 2);  // add extra bytes to account for different type sizes
+  unsigned int size;
+  PACK(p1, 2);
+  unsigned char hash[H];
 };
-struct image{
-    unsigned char sig[N];  // signature of header
-    unsigned char exp[N];
-    unsigned char mod[N];  // n in modexp
-    unsigned int num;      // total number of blocks
-    PACK(p0, 2);
-    struct modules module[1];
-};
-
-enum status{
-    UNDET,
-    PASS,
-    FAIL
+struct image {
+  unsigned char sig[N];  // signature of header
+  unsigned char exp[N];
+  unsigned char mod[N];  // n in modexp
+  unsigned int num;      // total number of blocks
+  PACK(p0, 2);
+  struct modules module[1];
 };
 
-//public key hash
-CODE unsigned char pkhash[20] = {0x37, 0x34, 0xA6, 0x83,
-				   0x5F, 0xFC, 0xE0, 0x2B,
-				   0xC6, 0xEE, 0xCB, 0x81,
-				   0x6C, 0x92, 0x6C, 0x7C,
-				   0xBA, 0x79, 0xCB, 0x8F};
+enum status { UNDET, PASS, FAIL };
 
+// public key hash
+CODE unsigned char pkhash[20] = {0x37, 0x34, 0xA6, 0x83, 0x5F, 0xFC, 0xE0, 0x2B, 0xC6, 0xEE,
+                                 0xCB, 0x81, 0x6C, 0x92, 0x6C, 0x7C, 0xBA, 0x79, 0xCB, 0x8F};
 
 void quit() {
-    P0 = P1 = P2 = P3 = 0xDE;
-    P0 = P1 = P2 = P3 = 0xAD;
-    P0 = P1 = P2 = P3 = 0x00;
-    while(1);
+  P0 = P1 = P2 = P3 = 0xDE;
+  P0 = P1 = P2 = P3 = 0xAD;
+  P0 = P1 = P2 = P3 = 0x00;
+  while (1)
+    ;
 }
-
 
 void fail(unsigned char* failmes) {
   (void)failmes;
   P0 = 0;
   quit();
-
 }
-
 
 void main() {
 
-    unsigned int i, j;
-    unsigned int num;   // total number of blocks
-    struct image* im;
-    struct modules* block; // current block
-    unsigned int size;
-    unsigned char* moddata;
-    unsigned int ldaddr = 0;
-    enum status pass = UNDET;
-    //unsigned char *m;
-    // STAGE 0: set up
-    //P0 = 5;
-    pt_init();
-    // set SHA read and write addresses
-    unlock(SHA, (unsigned char*)&sha_regs.rd_addr, (unsigned char*)(&sha_regs.wr_addr+1));
-    writeptr(SHA, &sha_regs.rd_addr, sha_in);
-    writeptr(SHA, &sha_regs.wr_addr, sha_out);
-    lock(SHA, (unsigned char*)&sha_regs.rd_addr, (unsigned char*)(&sha_regs.wr_addr+1));
-    // unlock memwr registers
-    unlock(MEMWR, (unsigned char*)&memwr_regs.start, (unsigned char*)(&memwr_regs+1));
+  unsigned int i, j;
+  unsigned int num;  // total number of blocks
+  struct image* im;
+  struct modules* block;  // current block
+  unsigned int size;
+  unsigned char* moddata;
+  unsigned int ldaddr = 0;
+  enum status pass = UNDET;
+  // unsigned char *m;
+  // STAGE 0: set up
+  // P0 = 5;
 
-    // set up RSA
-    unlock(RSA, (unsigned char*)&rsa_regs.opaddr, (unsigned char*)(&rsa_regs.opaddr+1));
+  record_dbg(VERIF_HEADER_SIGN, 0);
+  record_dbg(VERIF_BLOCK_HASH, 0);
 
-    writeptr(RSA, &rsa_regs.opaddr, rsa_out);  // set up address to write to
+  pt_init();
+  // set SHA read and write addresses
+  unlock(SHA, (unsigned char*)&sha_regs.rd_addr, (unsigned char*)(&sha_regs.wr_addr + 1));
+  writeptr(SHA, &sha_regs.rd_addr, sha_in);
+  writeptr(SHA, &sha_regs.wr_addr, sha_out);
+  lock(SHA, (unsigned char*)&sha_regs.rd_addr, (unsigned char*)(&sha_regs.wr_addr + 1));
+  // unlock memwr registers
+  unlock(MEMWR, (unsigned char*)&memwr_regs.start, (unsigned char*)(&memwr_regs + 1));
 
-    lock(RSA, (unsigned char*)&rsa_regs.opaddr, (unsigned char*)(&rsa_regs.opaddr+1));
+  // set up RSA
+  unlock(RSA, (unsigned char*)&rsa_regs.opaddr, (unsigned char*)(&rsa_regs.opaddr + 1));
 
+  writeptr(RSA, &rsa_regs.opaddr, rsa_out);  // set up address to write to
 
-    if(!RSAinit(rsa_out, sha_in, sha_out)){
-    	pass = FAIL;
-    	fail("invalid input/output addresses");
-    	return;
+  lock(RSA, (unsigned char*)&rsa_regs.opaddr, (unsigned char*)(&rsa_regs.opaddr + 1));
+
+  if (!RSAinit(rsa_out, sha_in, sha_out)) {
+    pass = FAIL;
+    fail("invalid input/output addresses");
+    return;
+  }
+
+  // STAGE 1: read image into RAM
+  unlock(pages[BOOT], boot, boot + MAX_IM_SIZE);
+  // load(0, MAX_IM_SIZE, boot, 0);
+  // image is loaded.
+  // now we need to lock boot to boot + MAX_IM_SIZE
+  lock(pages[BOOT], boot, boot + MAX_IM_SIZE);
+
+  // STAGE 2: check that key matches hash
+  im = (struct image*)boot;
+  // set signature key
+  unlock(RSA_KEYS, rsa_regs.exp, rsa_regs.exp + N);
+  load(im->exp, N, rsa_regs.exp, 0);
+
+  // writecarr(RSA_KEYS, rsa_regs.exp, im->exp, N);
+  lock(RSA_KEYS, rsa_regs.exp, rsa_regs.exp + N);
+  // set signature modulus
+  unlock(RSA_KEYS, rsa_regs.n, rsa_regs.n + N);
+  // writecarr(RSA_KEYS, rsa_regs.n, im->mod, N);
+  load(im->mod, N, rsa_regs.n, 0);
+  lock(RSA_KEYS, rsa_regs.n, rsa_regs.n + N);
+
+  // check the hashes
+  sha1(rsa_regs.exp, 2 * N);
+
+  for (i = 0; i < H; i++) {
+    if (sha_out[i] != pkhash[i]) {
+      pass = FAIL;  // FAIL: key hash mismatch
+      fail("key hash mismatch");
+      return;
     }
+  }
 
-    // STAGE 1: read image into RAM
-    unlock(pages[BOOT], boot, boot+MAX_IM_SIZE);
-    // load(0, MAX_IM_SIZE, boot, 0);
-    // image is loaded.
-    // now we need to lock boot to boot + MAX_IM_SIZE
-    lock(pages[BOOT], boot, boot+MAX_IM_SIZE);
+  // STAGE 3: verify signature in boot
+  num = im->num & 0xFFFF;  // number of modules
 
+  // sizeof image struct includes extra signature and first module
+  size = sizeof(struct image) - (im->exp - (unsigned char*)im) + sizeof(struct modules) * (num - 1);
 
-    // STAGE 2: check that key matches hash
-    im  = (struct image*) boot;
-    // set signature key
-    unlock(RSA_KEYS, rsa_regs.exp, rsa_regs.exp+N);
-    load(im->exp,N,rsa_regs.exp,0);
+  if (size > MAX_IM_SIZE) {
+    pass = FAIL;  // FAIL: image too large
+    fail("header too large");
+    return;
+  }
 
-    //writecarr(RSA_KEYS, rsa_regs.exp, im->exp, N);
-    lock(RSA_KEYS, rsa_regs.exp, rsa_regs.exp+N);
-    // set signature modulus
-    unlock(RSA_KEYS, rsa_regs.n, rsa_regs.n+N);
-    //writecarr(RSA_KEYS, rsa_regs.n, im->mod, N);
-    load(im->mod,N,rsa_regs.n,0);
-    lock(RSA_KEYS, rsa_regs.n, rsa_regs.n+N);
+  if (!verifySignature(im->exp, size, im->sig)) {
+    pass = FAIL;  // FAIL: signature mismatch
+    fail("signature mismatch");
+    return;
+  }
 
-    // check the hashes
-    sha1(rsa_regs.exp, 2*N);
+  // STAGE 4: load blocks
+  if (num == 0) {  // no blocks to load, done
+    pass = PASS;
+    return;
+  }
 
-    for(i=0; i<H; i++){
-    	if(sha_out[i] != pkhash[i]){
-    	    pass = FAIL;  // FAIL: key hash mismatch
-    	    fail("key hash mismatch");
-    	    return;
-    	}
-    }
+  unlock(pages[PRG], program, program + MAX_PRG_SIZE);  // unlock memory space for program
+  block = im->module;                                   // block data in header
+  moddata = (unsigned char*)(block + num);              // program data of this module
+  size = 0;
+  for (i = 0; i < num; i++) {
 
-    // STAGE 3: verify signature in boot
-    num = im->num & 0xFFFF;  // number of modules
-
-    // sizeof image struct includes extra signature and first module
-    size = sizeof(struct image) - (im->exp -(unsigned char*)im) + sizeof(struct modules) * (num-1);
-
-    if(size > MAX_IM_SIZE)
+    // check that size and address are valid
+    size = block->size & 0xFFFF;  // size of current module
+                                  // P1 = size;
+                                  // P1 = size>>8;
+    ldaddr = block->addr;         // address to load this module into
+                                  // P1 = ldaddr;
+                                  // P1 = ldaddr>>8;
+    // the data does not fit inside the image
+    if (moddata + size > boot + MAX_IM_SIZE || moddata + size < moddata)  // overflow
     {
-    	pass = FAIL; // FAIL: image too large
-    	fail("header too large");
-    	return;
+      // addr_val = 0;
+      pass = FAIL;
+      fail("data does not fit in image");
+      return;
+    }
+    // the data does not fit in memory range allocated for it
+    if (size + ldaddr > MAX_PRG_SIZE || ldaddr + size < ldaddr) {
+      // addr_val = 0;
+      pass = FAIL;
+      fail("program write out of range");
+      return;
     }
 
-
-    if(!verifySignature(im->exp, size, im->sig))
-    {
-    	pass = FAIL;  // FAIL: signature mismatch
-    	fail("signature mismatch");
-    	return;
-    }
-    // STAGE 4: load blocks
-    if(num == 0){  // no blocks to load, done
-    	pass = PASS;
-    	return;
+    // load data
+    if (size != 0) {
+      load(moddata, size, program + ldaddr, 0);
     }
 
-    unlock(pages[PRG], program, program + MAX_PRG_SIZE);  // unlock memory space for program
-    block = im->module;  // block data in header
-    moddata = (unsigned char*)(block + num); // program data of this module
-    size = 0;
-    for(i=0; i<num; i++)
-    {
+    // update to next module
+    moddata += size;
+    block++;
+  }
 
-      	// check that size and address are valid
-      	size = block->size & 0xFFFF;     // size of current module
-            //P1 = size;
-            //P1 = size>>8;
-      	ldaddr = block->addr;   // address to load this module into
-            //P1 = ldaddr;
-            //P1 = ldaddr>>8;
-      	// the data does not fit inside the image
-      	if(moddata + size > boot + MAX_IM_SIZE ||
-      	   moddata + size < moddata)  // overflow
-      	{
-      	    //addr_val = 0;
-      	    pass = FAIL;
-      	    fail("data does not fit in image");
-      	    return;
-      	}
-              // the data does not fit in memory range allocated for it
-      	if(size + ldaddr > MAX_PRG_SIZE ||
-      	   ldaddr + size < ldaddr)
-      	{
-      	    //addr_val = 0;
-      	    pass = FAIL;
-      	    fail("program write out of range");
-      	    return;
-      	}
+  // lock newly loaded data
+  lock(pages[PRG], program, program + MAX_PRG_SIZE);
 
-      	// load data
-      	if(size != 0){
-      	    load(moddata, size, program+ldaddr, 0);
-      	}
+  block = im->module;                       // go back to first module
+  moddata = (unsigned char*)(block + num);  // program data of this module
 
+  for (i = 0; i < num; i++) {
+    unsigned int ldaddr;
+    unsigned char* m;
+    size = block->size & 0xFFFF;  // size of current module
+    ldaddr = block->addr;         // address to load this module into
+    sha1(program + ldaddr, size);
 
-      	// update to next module
-      	moddata += size;
-      	block++;
+    m = program + ldaddr;
+
+    for (j = 0; j < H; j++) {
+      if (sha_out[j] != block->hash[j]) {
+        pass = FAIL;
+        fail("module hash mismatch");
+        return;
+      }
     }
 
+    // update to next module
+    moddata += size;
+    block++;
+  }
 
-
-    // lock newly loaded data
-    lock(pages[PRG], program, program + MAX_PRG_SIZE);
-
-    block = im->module;  // go back to first module
-    moddata = (unsigned char*)(block + num); // program data of this module
-
-    for(i=0; i<num; i++)
-    {
-        unsigned int ldaddr;
-        unsigned char *m;
-      	size = block->size & 0xFFFF;     // size of current module
-      	ldaddr = block->addr;   // address to load this module into
-	      sha1(program+ldaddr, size);
-
-        m = program+ldaddr;
-
-      	for(j=0; j<H; j++){
-      	    if(sha_out[j] != block->hash[j]){
-          		pass = FAIL;
-          		fail("module hash mismatch");
-          		return;
-      	    }
-	     }
-
-  	// update to next module
-      	moddata += size;
-      	block++;
+  // check that program loaded correctly, for testing only
+  for (i = 0; i < moddata - (unsigned char*)block; i++) {
+    if (program[i] != *((unsigned char*)block + i)) {
+      pass = FAIL;  // FAIL
+      fail("load is wrong");
+      break;
     }
+  }
 
-    // check that program loaded correctly, for testing only
-    for(i=0; i<moddata-(unsigned char*)block; i++){
-    	if(program[i] != *((unsigned char*)block + i)){
-    	    pass = FAIL;  // FAIL
-    	    fail("load is wrong");
-    	    break;
-    	}
-    }
+  // PASS or FAIL
+  if (pass != FAIL) {
+    pass = PASS;
+  }
+  P0 = pass;
 
-    // PASS or FAIL
-    if(pass != FAIL){
-	       pass = PASS;
-    }
-    P0 = pass;
+  record_dbg(VERIF_HEADER_SIGN, pass);
+  record_dbg(VERIF_BLOCK_HASH, pass);
 
-    quit();
+  quit();
 }
 
 // bootlock: image does not change once it is loaded
